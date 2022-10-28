4840c06bfce61bfa811633500efa74037d00c18e
Jeffrey Huynh <jthuynh@amazon.com>
2022-10-28 03:44:32 +0000

    Add phase2 BERT pretraining for pcluster example

--- torch-neuronx/training/dp_bert_hf_pretrain/run_dp_bert_large_hf_pretrain_bf16_s512_phase2.sh
+++ torch-neuronx/training/dp_bert_hf_pretrain/run_dp_bert_large_hf_pretrain_bf16_s512_phase2.sh
@@ -11,15 +11,20 @@ export NEURON_RT_STOCHASTIC_ROUNDING_SEED=0
 export TF_GRPC_DEFAULT_OPTIONS=grpc.keepalive_time_ms=60000,grpc.keepalive_timeout_ms=14400000,grpc.http2.max_pings_without_data=0,grpc.http2.min_ping_interval_without_data_ms=600000
 
 INSTANCEID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
+BATCH_SIZE=2
+GRAD_ACCUM_USTEPS=512
+SEQ_LEN=512
+MAX_PRED_LEN=80
+WARM_UP=781
+MAX_STEPS=1563
+PH1_END_STEP=28125 # the steps ran for phase1
+LR=2.8e-4
 WORLD_SIZE_JOB=1
 RANK_NODE=0
-MAX_STEPS=28125
-BATCH_SIZE=16
-GRAD_ACCUM_USTEPS=32
 NUM_NEURONCORES=32
 DISTRIBUTED_ARGS="--nproc_per_node $NUM_NEURONCORES"
 OUTPUT_DIR=output
-LOG_FILE=log_ph1_bf16
+LOG_FILE=log_ph2_bf16
 if [ ! -z "$NEURON_EXTRACT_GRAPHS_ONLY" ]; then
    LOG_FILE=${LOG_FILE}_compile
 fi
@@ -31,7 +36,7 @@ if [ ! -z "$SLURM_NTASKS" ]; then
     WORLD_SIZE_JOB=$SLURM_NTASKS
     RANK_NODE=$SLURM_NODEID
     MASTER_ADDR=(`scontrol show hostnames $SLURM_JOB_NODELIST`)
-    MASTER_PORT=2022
+    MASTER_PORT=2023
     GRAD_ACCUM_USTEPS=$(($GRAD_ACCUM_USTEPS/$WORLD_SIZE_JOB))
     DISTRIBUTED_ARGS="--nproc_per_node $NUM_NEURONCORES --nnodes $WORLD_SIZE_JOB --node_rank $RANK_NODE --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
     echo $DISTRIBUTED_ARGS
@@ -51,7 +56,21 @@ mkdir -p $OUTPUT_DIR
 if [ -z "$json" ]; then json="$OUTPUT_DIR/results.json" && rm -f $json; fi
 
 sudo sysctl -w net.ipv4.ip_local_reserved_ports=48620 || exit 1
-XLA_USE_BF16=1 torchrun $DISTRIBUTED_ARGS dp_bert_large_hf_pretrain_hdf5.py --output_dir $OUTPUT_DIR --steps_this_run $steps_this_run --metrics_file $json --batch_size=$BATCH_SIZE --grad_accum_usteps=$GRAD_ACCUM_USTEPS |& tee $OUTPUT_DIR/$LOG_FILE &
+XLA_USE_BF16=1 torchrun $DISTRIBUTED_ARGS dp_bert_large_hf_pretrain_hdf5.py \
+        --output_dir $OUTPUT_DIR \
+        --lr $LR \
+        --phase2 \
+        --resume_ckpt \
+        --resume_ckpt_path ../ckpt_28125.pt \
+        --phase1_end_step $PH1_END_STEP \
+        --batch_size $BATCH_SIZE \
+        --max_pred_len $MAX_PRED_LEN \
+        --data_dir ~/examples_datasets/bert_pretrain_wikicorpus_tokenized_hdf5_seqlen512/ \
+        --metrics_file $json \
+        --grad_accum_usteps $GRAD_ACCUM_USTEPS \
+        --max_steps $MAX_STEPS \
+        --steps_this_run $steps_this_run \
+        --warmup_steps $WARM_UP |& tee $OUTPUT_DIR/$LOG_FILE &
 wait %1
 
 ret_val=$?
