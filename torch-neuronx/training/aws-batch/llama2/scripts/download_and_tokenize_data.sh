4ed10c67dbfb1dad56fd8a31566f4f08e60a97e1
Sadaf Rasool <36799557+aws-sadaf@users.noreply.github.com>
2024-01-11 10:30:54 -0500

    Update download_and_tokenize_data.sh

--- torch-neuronx/training/aws-batch/llama2/scripts/download_and_tokenize_data.sh
+++ torch-neuronx/training/aws-batch/llama2/scripts/download_and_tokenize_data.sh
@@ -10,3 +10,4 @@ python3 get_dataset.py
 
 # pushing the tokenized dataset to predefined S3 location
 aws s3 cp ~/examples_datasets/wikicorpus_llama2_7B_tokenized_4k/ $TOKENIZED_DATASET_URI --recursive --only-show-errors
+echo "Dataset has been processed and tokenized data has been uploaded to $TOKENIZED_DATASET_URI successfully."
