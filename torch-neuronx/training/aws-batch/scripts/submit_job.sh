604b7c3420f763ff12830faec19120371ed32f0b
aws-mesharma <66804068+aws-mesharma@users.noreply.github.com>
2024-02-07 10:07:39 -0800

    Delete torch-neuronx/training/aws-batch/scripts directory

--- torch-neuronx/training/aws-batch/scripts/submit_job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-# submitting aws batch job
-aws batch submit-job \
-    --job-name aws-batch-trn1-job \
-    --job-queue aws-batch-job-queue \
-    --job-definition aws-batch-job-definition \
-    --node-overrides numNodes=4
