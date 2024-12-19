d6e1754d2e65c90938565e3d23a814f2f800cc21
Jeffrey Huynh <jthuynh@amazon.com>
2024-12-19 20:52:20 +0000

    Remove lnc_count

--- torch-neuronx/training/dp_bert_hf_pretrain/run_dp_bert_large_hf_pretrain_bf16_s128.sh
+++ torch-neuronx/training/dp_bert_hf_pretrain/run_dp_bert_large_hf_pretrain_bf16_s128.sh
@@ -42,18 +42,16 @@ fi
 
 if [ -e /opt/aws/neuron/bin/neuron-ls ]; then
     NUM_DEVICES=`/opt/aws/neuron/bin/neuron-ls -j | jq '. | length'`
-    NC_PER_DEVICE=`/opt/aws/neuron/bin/neuron-ls -j | jq '.[0].lnc_count'`
-    echo "Found logical $NC_PER_DEVICE nc per device"
+    NC_PER_DEVICE=`/opt/aws/neuron/bin/neuron-ls -j | jq '.[0].nc_count'`
+    echo "Found $NC_PER_DEVICE NeuronCores per device"
 
-    if [ -z "$NC_PER_DEVICE" ] || [ "$NC_PER_DEVICE" == "null" ]; then
-        NC_PER_DEVICE=`/opt/aws/neuron/bin/neuron-ls -j | jq '.[0].nc_count'`
-        if [[ "$NC_PER_DEVICE" == "8" || "$NC_PER_DEVICE" == "128" ]]; then
-            echo " Running on Trn2 device"
-            let NC_PER_DEVICE=$NC_PER_DEVICE/2
-        fi
+    if [ -z "$NUM_DEVICES" ] || [ "$NUM_DEVICES" == "0" ] || [ -z "$NC_PER_DEVICE" ] || [ "$NC_PER_DEVICE" == "null" ]; then
+        NUM_NEURONCORES=32
+        echo "Unable to extract device count and nc_count from neuron-ls json output; using default $NUM_NEURONCORES NeuronCores"
+    else
+        let NUM_NEURONCORES=$NUM_DEVICES*$NC_PER_DEVICE
+        echo "Found $NUM_NEURONCORES NeuronCores"
     fi
-    let NUM_NEURONCORES=$NUM_DEVICES*$NC_PER_DEVICE
-    echo "Found $NUM_NEURONCORES NeuronCores"
 else
     NUM_NEURONCORES=32
     echo "neuron-ls not installed (aws-neuronx-tools); using default $NUM_NEURONCORES NeuronCores"
