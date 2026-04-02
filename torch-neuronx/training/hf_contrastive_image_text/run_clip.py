a9e84447e4ecd78c79c314f4dd12ca8ac24b4ed0
Doug Erickson (AWS) <dougeric@amazon.com>
2026-04-02 15:06:07 -0700

    Added note on end of TorchXLA support to affected samples

--- torch-neuronx/training/hf_contrastive_image_text/run_clip.py
+++ torch-neuronx/training/hf_contrastive_image_text/run_clip.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# IMPORTANT: Neuron will stop supporting XLA-based training support in a future release. For now, this code sample is provided strictly for reference.
 # coding=utf-8
 # Copyright 2022 The HuggingFace Team All rights reserved.
 #
